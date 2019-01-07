24466597c73a45e8cd05aaa97d86fb906ef4fb5e
Madan Bhandari <madan.kbh@gmail.com>
2019-01-07 15:41:04 +0530

    Problem : Home page is blank #10
    Solution : Projects added on home page

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -1,2 +1,28 @@
-import "./home.html";
-import "./home.scss";
+import "./home.html"
+import "./home.scss"
+import "../../shared/searchBar/searchBar"
+import "../../shared/searchResults/searchResults"
+
+Template.home.onCreated(function() {
+  this.searchFilter = new ReactiveVar(undefined);
+})
+
+Template.home.helpers({
+  searchArgs : () => {
+    const instance = Template.instance();
+    return {
+      placeholder: "Search projects",
+      type: "projects",
+      onChange: newTerm => instance.searchFilter.set(newTerm),
+    };
+  },
+
+  resultArgs : () => {
+    return {
+      types: ["projects"],
+      searchTerm: Template.instance().searchFilter.get(),
+      doLanguageGrouping: true,
+      languages: Meteor.user() && Meteor.user().profile && Meteor.user().profile.contentLanguages,
+    };
+  },
+})
