3f87543f0a40a69f2d0bf4346d22ca5710201652
Dilpreet Singh <singhsince94@gmail.com>
2019-01-11 14:00:15 +0530

    Problem: no alerts of form validation for project form #17
    Solution: Add recaptcha to the api which fixes the errors in forms.

--- imports/startup/server/startup.js
+++ imports/startup/server/startup.js
@@ -1,14 +1,15 @@
 import { Meteor } from "meteor/meteor";
+import { Config } from '/imports/api/config/config'
 
 Meteor.startup(() => {
   SyncedCron.start();
 
-  let config = {};
-  // Config.findOne({
-  //   _id: "recaptcha",
-  // }) || {};
+  let config = Config.findOne({
+    _id: "recaptcha",
+  }) || {};
 
   const isTest = Meteor.isAppTest || Meteor.isTest;
+
   Meteor.settings.public.RECAPTCHA_CLIENT = isTest
     ? "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
     : config.publicKey || "";
@@ -16,4 +17,5 @@ Meteor.startup(() => {
   reCAPTCHA.config({
     privatekey: isTest ? "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe" : config.privateKey || "",
   });
+
 });
